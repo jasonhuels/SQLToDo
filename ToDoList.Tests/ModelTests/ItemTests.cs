453b8b7c9dfac97544c88cfc185eb7c78afa2fb2
Student <example@example.com>
2019-10-14 11:34:38 -0700

    make project

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using ToDoList.Models;
 using System;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Tests
 {
@@ -12,6 +13,12 @@ namespace ToDoList.Tests
     public void Dispose()
     {
       Item.ClearAll();
+    }
+
+    // The method below is new code.
+    public ItemTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
     }
 
     [TestMethod]
@@ -36,6 +43,32 @@ namespace ToDoList.Tests
     }
 
     [TestMethod]
+    public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+    {
+      // Arrange, Act
+      Item firstItem = new Item("Mow the lawn");
+      Item secondItem = new Item("Mow the lawn");
+
+      // Assert
+      Assert.AreEqual(firstItem, secondItem);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_ItemList()
+    {
+      //Arrange
+      Item testItem = new Item("Mow the lawn");
+
+      //Act
+      testItem.Save();
+      List<Item> result = Item.GetAll();
+      List<Item> testList = new List<Item>{testItem};
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
     public void SetDescription_SetDescription_String()
     {
       //Arrange
@@ -52,15 +85,15 @@ namespace ToDoList.Tests
     }
 
     [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
+    public void GetAll_ReturnsEmptyListFromDatabase_ItemList()
     {
-      // Arrange
+      //Arrange
       List<Item> newList = new List<Item> { };
 
-      // Act
+      //Act
       List<Item> result = Item.GetAll();
 
-      // Assert
+      //Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
@@ -71,7 +104,9 @@ namespace ToDoList.Tests
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
       Item newItem1 = new Item(description01);
+      newItem1.Save(); // New code
       Item newItem2 = new Item(description02);
+      newItem2.Save(); // New code
       List<Item> newList = new List<Item> { newItem1, newItem2 };
 
       //Act
@@ -84,9 +119,11 @@ namespace ToDoList.Tests
     [TestMethod]
     public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
     {
+      Item.ClearAll();
       //Arrange
       string description = "Walk the dog.";
       Item newItem = new Item(description);
+      newItem.Save();
 
       //Act
       int result = newItem.Id;
@@ -96,19 +133,18 @@ namespace ToDoList.Tests
     }
 
     [TestMethod]
-    public void Find_ReturnsCorrectItem_Item()
+    public void Find_ReturnsCorrectItemFromDatabase_Item()
     {
       //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
+      Item newItem = new Item("Mow the lawn");
+      newItem.Save();
+      Item newItem2 = new Item("Wash dishes");
+      newItem2.Save();
 
       //Act
-      Item result = Item.Find(2);
-
+      Item foundItem = Item.Find(newItem.Id);
       //Assert
-      Assert.AreEqual(newItem2, result);
+      Assert.AreEqual(newItem, foundItem);
     }
   }
 }
